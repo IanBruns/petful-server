681a08c04e552a31ce233a02d3c54f7e8982b6fe
Ian Bruns <ian@rbruns.com>
2021-02-19 10:17:18 -0700

    people service connected

--- modules/people/people.router.js
+++ modules/people/people.router.js
@@ -6,7 +6,8 @@ const People = require('./people.service');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // Return all the people currently in the queue.
+  const allPeople = People.get();
+  return res.status(200).json(allPeople);
 });
 
 router.post('/', json, (req, res) => {
